a64dee8c2175e6aec5f5ad34f443b2eee1327fe2
unknown <mckennar@udel.edu>
2015-05-14 18:00:37 -0400

    wrote sequential lu functions (vectorize, loop unroll), updated benchmarking code

--- lu.c
+++ lu.c
@@ -3,6 +3,8 @@
 #include <math.h>
 #include <time.h>
 #include <omp.h>
+
+typedef void (solver)(double**, double**, double**, int);
  
 void zero(double **A, int n) { 
 	for(int i=0; i < n; i++)
@@ -67,7 +69,9 @@ void lu(double **A, double **L, double **U, int n) {
 		}
 	}
 }
-
+/**
+* GCC: gcc -o lu -fopenmp -std=c99 lu.c
+*/
 void luParallel(double **A, double **L, double **U, int n) {
 	zero(L,n); zero(U,n);
 	
@@ -80,11 +84,77 @@ void luParallel(double **A, double **L, double **U, int n) {
 	
 	for(int j=0; j < n; j++){
 		// this loop can be parallelized
-		#pragma omp for schedule(static, 8) 
+		#pragma omp parallel for schedule(static, 8) 
 		for(int i=j+1; i < n; i++) {
 			double m = U[i][j] / U[j][j];
 			L[i][j] = m;
 			// this loop can also be parallelized
+			for(int k=j; k < n; k++)
+				U[i][k] -= m*U[j][k];
+		}
+	}
+}
+
+void luAll(double **A, double **L, double **U, int n) {
+	zero(L,n); zero(U,n);
+	
+	for(int i=0; i < n; i++)
+		for(int j=0; j < n; j++)
+			U[i][j] = A[i][j];
+ 
+	for(int i=0; i < n; i++)
+		L[i][i] = 1;
+	
+	for(int j=0; j < n; j++){
+		#pragma omp parallel for schedule(static, 8) 
+		for(int i=j+1; i < n; i++) {
+			double m = U[i][j] / U[j][j];
+			L[i][j] = m;
+			#pragma vector always
+			#pragma unroll 4
+			for(int k=j; k < n; k++)
+				U[i][k] -= m*U[j][k];
+		}
+	}
+}
+
+void luUnroll(double **A, double **L, double **U, int n) {
+	zero(L,n); zero(U,n);
+	
+	for(int i=0; i < n; i++)
+		for(int j=0; j < n; j++)
+			U[i][j] = A[i][j];
+ 
+	for(int i=0; i < n; i++)
+		L[i][i] = 1;
+	
+	for(int j=0; j < n; j++){
+		for(int i=j+1; i < n; i++) {
+			double m = U[i][j] / U[j][j];
+			L[i][j] = m;
+			#pragma unroll 4
+			for(int k=j; k < n; k++)
+				U[i][k] -= m*U[j][k];
+		}
+	}
+}
+
+
+void luVec(double **A, double **L, double **U, int n) {
+	zero(L,n); zero(U,n);
+	
+	for(int i=0; i < n; i++)
+		for(int j=0; j < n; j++)
+			U[i][j] = A[i][j];
+ 
+	for(int i=0; i < n; i++)
+		L[i][i] = 1;
+	
+	for(int j=0; j < n; j++){
+		for(int i=j+1; i < n; i++) {
+			double m = U[i][j] / U[j][j];
+			L[i][j] = m;
+			#pragma vector always
 			for(int k=j; k < n; k++)
 				U[i][k] -= m*U[j][k];
 		}
@@ -97,7 +167,7 @@ void luParallel(double **A, double **L, double **U, int n) {
 * Intel compiler: icc -xHOST -O3 
 * GCC: gcc -march=native -O3
 */
-void lu_v2(double **A, double **L, double **U, int n) {
+void luUnrollVec(double **A, double **L, double **U, int n) {
 	zero(L,n); zero(U,n);
 	
 	for(int i=0; i < n; i++)
@@ -108,7 +178,6 @@ void lu_v2(double **A, double **L, double **U, int n) {
 		L[i][i] = 1;
 	
 	for(int j=0; j < n; j++){
-		// this loop can be parallelized
 		for(int i=j+1; i < n; i++) {
 			double m = U[i][j] / U[j][j];
 			L[i][j] = m;
@@ -120,10 +189,11 @@ void lu_v2(double **A, double **L, double **U, int n) {
 	}
 }
 
-void check_LU(double **A, int n) {
+void check_LU(solver *f, int n) {
+	double **A = rand_matrix(n);
 	double **L = new_matrix(n);
 	double **U = new_matrix(n);
-	lu(A,L,U,n);
+	(*f)(A,L,U,n);
 	double **LU = new_matrix(n);
 	mult(L,U,LU,n);
 	for(int i=0; i < n; i++)
@@ -132,37 +202,46 @@ void check_LU(double **A, int n) {
 				printf("Test Failed!! \n");
 				show(A,n); show(L,n); show(U,n);
 			}
+	free(L); free(U); free(A);
 	printf("Test Passed!! \n");
 }
 
-void benchmark_LU(int n, int trials){
+float benchmark_LU(solver *f, int n, int trials){
 	clock_t t = 0;
 	double **L = new_matrix(n);
 	double **U = new_matrix(n);
 	for(int i=0; i < trials; i++) {
 		double **A = rand_matrix(n);
 		t -= clock();
-		luParallel(A,L,U,n);
+		(*f)(A,L,U,n);
 		t += clock();
 		free(A);
 	}
 	free(L); free(U);
-	printf( "A matrix of size %d x %d took an average of %f seconds to factor.\n", n, n, ((float)t)/(trials*CLOCKS_PER_SEC)); 
+	//printf( "A matrix of size %d x %d took an average of %f seconds to factor.\n", n, n, ((float)t)/(trials*CLOCKS_PER_SEC)); 
+	return (float) t/(trials*CLOCKS_PER_SEC);
 }
 
 void benchmark() {
-	for(int n=64; n <= 2048; n*=2) {
-		benchmark_LU(n, 1);
+	solver *solvers[6] = { &lu, &luVec, &luUnroll, &luUnrollVec, &luParallel, &luAll };
+	for(int i=0; i < 6; i++)
+		check_LU(solvers[i],25);	
+	
+	printf("n, lu, luVec, luUnroll, luVecUnroll, luParallel, luAll\n");
+	for(int n=64; n <= 4096; n*=2) {
+		printf("%d, ", n);
+		for(int s=0; s < 6; s++) {
+			float t = benchmark_LU(solvers[s], n, 1);
+			printf("%f, ",t);
+		}
+		printf("\n");
 	}
 }
+
+	
   
 int main() {
-	double **A = rand_matrix(3);
-	double **L = new_matrix(3);
-	double **U = new_matrix(3);
-	
-	lu(A,L,U,3);
-	check_LU(A,3);
+	check_LU(&lu,3);
 
 	//show(A,3);
 	//show(L,3);
